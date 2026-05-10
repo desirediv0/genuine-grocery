e086b8204cfd5a0034e6b4ac02ad2b5d76d7326a
riteshk-007 <codeshorts007@gmail.com>
2026-05-10 19:29:38 +0530

    feat: initialize vite config with react, tailwindcss, path aliases, and host configurations

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   },
   // Add the preview configuration for production builds
   preview: {
-    port: 4182,
+    port: 4183,
     host: "0.0.0.0",
     allowedHosts: [
       "admin.genuinegrocery.in",
